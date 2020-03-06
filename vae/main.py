0bd8db8d718a9b0e73adfd510eeb4c5bfc966cbd
Ben Hayes <ben@benhayes.net>
2020-03-06 10:33:11 +0000

    Switch to convolutional VAE

--- vae/main.py
+++ vae/main.py
@@ -39,16 +39,17 @@ test_loader = torch.utils.data.DataLoader(
 class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
+        
+        self.conv1 = nn.Conv2d(1, 2, 5, padding=2)
+        self.conv21 = nn.Conv2d(2, 4, 5, padding=2)
+        self.conv22 = nn.Conv2d(2, 4, 5, padding=2)
 
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.conv3 = nn.Conv2d(4, 2, 5, padding=2)
+        self.conv4 = nn.Conv2d(2, 1, 5, padding=2)
 
     def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        h1 = F.relu(self.conv1(x))
+        return self.conv21(h1), self.conv22(h1)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
@@ -56,11 +57,11 @@ class VAE(nn.Module):
         return mu + eps*std
 
     def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        h3 = F.relu(self.conv3(z))
+        return torch.sigmoid(self.conv4(h3))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
+        mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
@@ -126,7 +127,7 @@ if __name__ == "__main__":
         train(epoch)
         test(epoch)
         with torch.no_grad():
-            sample = torch.randn(64, 20).to(device)
+            sample = torch.randn(64, 4, 28, 28).to(device)
             sample = model.decode(sample).cpu()
             save_image(sample.view(64, 1, 28, 28),
                        'results/sample_' + str(epoch) + '.png')
