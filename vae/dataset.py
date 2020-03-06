5e66c3b51b3b946a852c08a185f953b8fb908cdf
Ben Hayes <ben@benhayes.net>
2020-03-06 13:09:07 +0000

    Update DS code

--- vae/dataset.py
+++ vae/dataset.py
@@ -2,6 +2,7 @@ import math
 import os
 
 import librosa
+import numpy as np
 from torch.utils.data import Dataset
 
 FFT_SIZE = 1024
@@ -9,15 +10,15 @@ HOP_SIZE = 512
 
 class SingingData(Dataset):
     def __init__(self, file_dir):
-        self._files = self._mine_files(file_dir)
+        self._files = mine_files(file_dir)
 
     def __len__(self):
         return len(self._files)
 
     def __getitem__(self, i):
         file_path = self._files[i]
-        x, sr = librosa.load(file_path)
-        spec = 0
+        spec = np.load(file_path)
+        return tf.tensor(spec).float()
 
 
 def make_spectrogram(audio):
@@ -25,6 +26,7 @@ def make_spectrogram(audio):
 
 
 def preprocess_data(audio_file):
+    print("Processing %s" % (audio_file))
     x, sr = librosa.load(audio_file)
 
     chop_length = sr * 2
@@ -32,21 +34,21 @@ def preprocess_data(audio_file):
     chunks = []
 
     n_chunks = math.ceil(x.shape[0] / chop_length)
-    for n in n_chunks:
+    for n in range(n_chunks):
         chunks.append(zero_pad(x[n * chop_length:(n + 1) * chop_length], chop_length))
 
     spectrograms = [ ]
     for chunk in chunks:
-        spectrograms.append(make_spectrogram(audio))
+        spectrograms.append(make_spectrogram(chunk))
 
     return spectrograms
 
 
 def zero_pad(arr, to):
     x = arr.copy()
-    if x.shape[0] < chop_length:
-        diff = x.shape[0] - chop_length
-        np.pad(x, (0, diff), 'constant', contant_values=0)
+    if x.shape[0] < to:
+        diff = to - x.shape[0]
+        np.pad(x, (0, diff), 'constant', constant_values=0)
         return x
     else:
         return x[:to]
